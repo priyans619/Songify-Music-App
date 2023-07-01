ad87ed782cabb4ffb063c2d3038b87164ac1e0d0
Priyansh Bhardwaj <priyansh.2b@gmail.com>
2023-07-01 21:23:14 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -29,7 +29,7 @@ const Sidebar = () => {
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#1a1a1a]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-        <h2 className="font-bold text-xl text-white text-center mt-2">Music App Clone</h2>
+        <h2 className="font-bold text-xl text-white text-center mt-2"></h2>
         <NavLinks />
       </div>
 
