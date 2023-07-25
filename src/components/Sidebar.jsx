2d3dd8450de8187d9eefa1ecd63edd442535c5c0
priyans619 <priyansh.2b@gmail.com>
2023-07-25 21:43:42 +0530

    Add screenshot and updated slidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -29,7 +29,7 @@ const Sidebar = () => {
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#1a1a1a]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-        <h2 className="font-bold text-xl text-white text-center mt-2">Music App Clone</h2>
+        <h2 className="font-bold text-xl text-white text-center mt-2"></h2>
         <NavLinks />
       </div>
 
@@ -41,11 +41,10 @@ const Sidebar = () => {
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`} >
         <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-        <h2 className="font-bold text-xl text-white text-center mt-2">Music App Clone</h2>
+        <h2 className="font-bold text-xl text-white text-center mt-2"></h2>
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
 };
-
 export default Sidebar;
